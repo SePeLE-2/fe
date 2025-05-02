294086b7dd1b0528d477b652110b7d31093ae5fa
Muhammad Madhani Putra <muhammad.madhani@ui.ac.id>
2025-05-02 23:52:12 +0700

    feat: Event ticket and report generated FE

---
+++ src/commons/utils/isSelectedFeature.js
@@ -0,0 +1,14 @@
+const isSelectedFeature = (item) => selectedFeatures.some(x => x.includes(item))
+
+export default isSelectedFeature
+
+var selectedFeatures = Array.from(new Set ([
+
+"NewsArticle",
+
+
+"Comment",
+
+
+"Home",
+]))
