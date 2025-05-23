39ce07bbd2f1e8096a87f0118db452a199de3453
AkubaruF <112474451+AkubaruF@users.noreply.github.com>
2025-05-23 18:44:41 +0700

    feat: add payment page

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,20 +6,22 @@ import staticPageRoutes from "@/staticPage/routes";
 import homeRoutes from "@/home/routes";
 import eventRoutes from "@/event/routes";
 import newsArticleRoutes from "@/newsArticle/routes";
+import paymentRoutes from "@/payment/routes";
 // import commentRoutes from "./comment/routes";
 import reportRoutes from "./report/routes";
 import ticketRoutes from "./ticket/routes";
 
 const GlobalRoutes = () => {
   const router = useRoutes([
-	...commonRoutes,
-	...staticPageRoutes,
-	...userRoutes,
-	...roleRoutes,
-	...homeRoutes, 
+    ...commonRoutes,
+    ...staticPageRoutes,
+    ...userRoutes,
+    ...roleRoutes,
+    ...homeRoutes,
     ...eventRoutes,
     ...reportRoutes,
     ...newsArticleRoutes,
+    ...paymentRoutes,
     ...ticketRoutes,
     // ...commentRoutes,
   ])
@@ -27,10 +29,10 @@ const GlobalRoutes = () => {
 }
 
 const MobileRoutes = () => {
-	const router = useRoutes([ 
-	  ...commonMobileRoutes, 
+  const router = useRoutes([
+    ...commonMobileRoutes,
   ])
   return router
 }
 
-export {GlobalRoutes, MobileRoutes}
+export { GlobalRoutes, MobileRoutes }
